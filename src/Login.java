da9d61fbd1642e26affbb1d17f6a48a7652b02e8
Aigerim <aigerimkadyrbaeva92@gmail.com>
2020-07-30 23:53:40 -0500

    add Login.java

---
+++ src/Login.java
@@ -0,0 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("It is my pleasure to be with you");
+    }
+}
