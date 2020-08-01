82e9f715824af2325640b2d8f11821d035f7d1e6
Aigerim <aigerimkadyrbaeva92@gmail.com>
2020-08-01 10:34:24 -0500

    add HappyB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,6 +1,6 @@
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
-public class Login {
+public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("It is my pleasure to be with you");
     }
