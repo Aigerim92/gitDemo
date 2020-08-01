e6a3557150a654bb8e95562854c82fb341a790cc
Aigerim <68886073+Aigerim92@users.noreply.github.com>
2020-08-01 17:27:16 -0500

    Update call.java

--- src/call.java
+++ src/call.java
@@ -2,5 +2,7 @@ public class call {
     public static void main(String[] args) {
         System.out.println( "call to Muhtar");
         //clean
+        
+        //i will see you in IntelliJ
     }
 }
